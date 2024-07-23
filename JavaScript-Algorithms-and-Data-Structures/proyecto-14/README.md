3d5c3543bbf99b4d208fc2bd51b1e96a6290a8d6
crcascr <cristianj_20@hotmail.com>
2024-07-23 16:35:28 -0500

    Adicion proyecto 14

---
+++ JavaScript-Algorithms-and-Data-Structures/proyecto-14/README.md
@@ -0,0 +1,15 @@
+# Learn Advanced Array Methods by Building a Statistics Calculator
+
+## Description
+
+As you expand your JavaScript skills, you'll want to get comfortable with array manipulation methods, such as `map()`, `reduce()`, and `filter()`.
+
+In this statistics calculator project, you'll gain experience with handling user input, DOM manipulation, and method chaining. You'll get practice by performing statistical calculations like mean, median, mode, variance, and standard deviation.
+
+## Acknowledgments
+
+This project is part of the [freeCodeCamp JavaScript and Data Structures certificate.](https://www.freecodecamp.org/learn/learn-javascript-algorithms-and-data-structures)
+
+## Contributing
+
+Contributions are welcome! If you find any issues or have suggestions for improvements, please open an issue or submit a pull request.
---
+++ JavaScript-Algorithms-and-Data-Structures/proyecto-14/script.js
@@ -0,0 +1,80 @@
+const getMean = (array) =>
+  array.reduce((acc, el) => acc + el, 0) / array.length;
+
+/*
+const testArr1 = [1, 2, 3, 4, 5];
+const testArr2 = [1, 2, 3, 4, 5, 6];
+const isEven = testArr2.length % 2 === 0;
+console.log(isEven);
+const oddListMedian = testArr1[Math.floor(testArr1.length / 2)];
+console.log(oddListMedian);
+const evenListMedian = getMean([
+  testArr2[testArr2.length / 2],
+  testArr2[testArr2.length / 2 - 1],
+]);
+console.log(evenListMedian);
+*/
+
+const getMedian = (array) => {
+  const sorted = array.slice().sort((a, b) => a - b);
+  const median =
+    array.length % 2 === 0
+      ? getMean([sorted[array.length / 2], sorted[array.length / 2 - 1]])
+      : sorted[Math.floor(array.length / 2)];
+  return median;
+};
+
+const getMode = (array) => {
+  const counts = {};
+  array.forEach((el) => (counts[el] = (counts[el] || 0) + 1));
+  if (new Set(Object.values(counts)).size === 1) {
+    return null;
+  }
+  const highest = Object.keys(counts).sort((a, b) => counts[b] - counts[a])[0];
+  const mode = Object.keys(counts).filter(
+    (el) => counts[el] === counts[highest]
+  );
+  return mode.join(", ");
+};
+
+const getRange = (array) => {
+  return Math.max(...array) - Math.min(...array);
+};
+
+const getVariance = (array) => {
+  const mean = getMean(array);
+  //const differences = array.map((el) => el - mean);
+  //const squaredDifferences = differences.map((el) => el ** 2);
+  //const sumSquaredDifferences = squaredDifferences.reduce((acc, el) => acc + el,0);
+  const variance =
+    array.reduce((acc, el) => {
+      const difference = el - mean;
+      const squared = difference ** 2;
+      return acc + squared;
+    }, 0) / array.length;
+  return variance;
+};
+
+const getStandardDeviation = (array) => {
+  const variance = getVariance(array);
+  const standardDeviation = Math.sqrt(variance);
+  return standardDeviation;
+};
+
+const calculate = () => {
+  const value = document.querySelector("#numbers").value;
+  const array = value.split(/,\s*/g);
+  const numbers = array.map((el) => Number(el)).filter((el) => !isNaN(el));
+  const mean = getMean(numbers);
+  const median = getMedian(numbers);
+  const mode = getMode(numbers);
+  const range = getRange(numbers);
+  const variance = getVariance(numbers);
+  const standardDeviation = getStandardDeviation(numbers);
+  document.querySelector("#median").textContent = median;
+  document.querySelector("#mean").textContent = mean;
+  document.querySelector("#mode").textContent = mode;
+  document.querySelector("#range").textContent = range;
+  document.querySelector("#variance").textContent = variance;
+  document.querySelector("#standardDeviation").textContent = standardDeviation;
+};
